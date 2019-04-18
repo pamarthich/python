dc1a90ef045313e59ae0906ddd61518bccb88ef7
root <root@ip-172-31-20-81.ap-south-1.compute.internal>
2019-04-18 20:13:15 +0000

    added parameters.tf file

---
+++ parameters.tf
@@ -0,0 +1,14 @@
+parameter {
+    name  = "slow_query_logs"
+    value = "1"
+  }
+
+  parameter {
+    name  = "general_log"
+    value = "1"
+  }
+
+  parameter {
+    name  = "long_query_time"
+    value = "2"
+  }
